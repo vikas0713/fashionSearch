b294e5ac90eb7e66537d7bb5bfdbe772ba3ba005
vikas <vikas0713@gmail.com>
2015-02-06 15:53:57 +0530

    standalone pages

--- fashionSearch/urls.py
+++ fashionSearch/urls.py
@@ -7,5 +7,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'fashionSearch.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^$',index),
+    url(r'^about/',about),
+    url(r'^contact/',contact),
     url(r'^admin/', include(admin.site.urls)),
 )
--- fashionSearch/views.py
+++ fashionSearch/views.py
@@ -4,3 +4,9 @@ from django.http import HttpResponse
 
 def index(request):
     return render_to_response('index.html')
+
+def about(request):
+    return render_to_response('about.html')
+
+def contact(request):
+    return render_to_response('contact.html')
